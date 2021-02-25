d0bab96e08aa20ca69ccee62a3c575053b32981a
vitaly97w <vitaly97w@gmail.com>
2021-02-25 22:20:35 +1000

    create methods drop, dropRight, dropRightWhile

--- myArrays.js
+++ myArrays.js
@@ -1,27 +1,22 @@
 class myArrays {
-  constructor(...arr) {
-    this.array = arr
-  }
-
+  array = []
   write() {
     console.log(this.array)
   }
   //Разбивает массив на подмассивы заданной длины
-  chunk(size = 1) {
+  chunk(array, size = 1) {
     const arr = []
-    const len = this.array.length / size
+    const len = array.length / size
     for (let i = 0; i < len; i++) {
-      arr.push(this.array.slice(0, size))
-      this.array.splice(0, size)
+      arr.push(array.slice(0, size))
     }
     return arr
   }
   //Удаляет все ложные элементы(false, null, NaN, "", undefined, 0)
-
-  compact() {
+  compact(array) {
     const arr = []
 
-    for (let val of this.array) {
+    for (let val of array) {
       if (
         val !== undefined &&
         isNaN(val) === false &&
@@ -35,7 +30,54 @@ class myArrays {
     }
     return arr
   }
+  //ВЫтаскивает значения из вложенных массивов и возвращет массив с этими значениями
+  concat(...arr) {
+    let valArr
+    for (let index in arr) {
+      if (Array.isArray(arr[index])) {
+        valArr = arr[index]
+        arr.splice(0, index)
+        this.array.push(valArr[0])
+      } else {
+        this.array.push(arr[index])
+      }
+    }
+    return this.array
+  }
+  //удаляет n данных из начала массива и возващает новый массив
+  drop(array, n = 1) {
+    for (let i = 0; i < n; i++) {
+      array.shift()
+    }
+    return array
+  }
+  //удаляет n данных из конца массива и возващает новый массив
+  dropRight(array, n = 1) {
+    for (let i = 0; i < n; i++) {
+      array.pop()
+    }
+    return array
+  }
+
+  dropRightWhile(arr, pred) {
+    const objR = []
+    for (let i = 0; i < arr.length; i++) {
+      for (let key in arr[i]) {
+        if (arr[i][key] === pred[key]) {
+          arr.pop()
+        }
+      }
+    }
+    return arr
+  }
 }
 
-const arr = new myArrays(false, 1, 0, 4, NaN, undefined, null, 3, '')
-console.log(arr.compact())
+let users = [
+  { user: 'barney', active: false },
+  { user: 'pebbles', active: true },
+  { user: 'pebbles', active: false },
+  { user: 'pebbles', active: false },
+]
+
+const arr = new myArrays()
+console.log(arr.dropRightWhile(users, { active: false }))
